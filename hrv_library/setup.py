962dc6d5969c0440e86aae9e03b18799aa475c82
robinchampseix <robin.champseix@gmail.com>
2018-09-26 18:21:47 +0200

    modified notebooks for exploration of data

--- hrv_library/setup.py
+++ hrv_library/setup.py
@@ -1,21 +1,20 @@
-from codecs import open
-from setuptools import setup, find_packages
+import setuptools
 
 # Get long description in READ.md file
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
     name="hrvanalysis",
     version="0.0.1",
     author="Robin Champseix",
-    license="GPL",
+    license="GPLv3",
     author_email="robin.champseix@gmail.com",
     description="a package to calculate features from Rr Interval for HRV analyses",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Aura-healthcare/Aura_data_analysis_platform",
-    packages=find_packages(),
+    url="https://github.com/robinchampseix/hrvanalysis",
+    packages=setuptools.find_packages(),
     install_requires=[
         "astropy>=3.0.4",
         "future>=0.16.0",
